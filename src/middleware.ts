6febd2cdca0a00b0d0efd7af42be0f249d73ab82
Heria021 <hariomsuthar7143@gmail.com>
2024-07-31 21:49:55 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,11 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}; 
+};
